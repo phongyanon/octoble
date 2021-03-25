db5c4aa55c10bd6a6babc48c894b913fc88300e1
phonyanon <phonyanon@nexpie.com>
2021-03-25 14:51:58 +0700

    update for test

--- octoble.js
+++ octoble.js
@@ -191,11 +191,11 @@ DeviceHandler.prototype.readCharacteristic = function(charuuid) {
             }
         }
         alert('test', charuuid);
-        // setTimeout(function() {
-        //     waiting = false;
-        //     emitter.off('read_characteristic', cb);
-        //     reject({code:408, result:'read_characteristic Timeout'});
-        // }, that.option.read_timeout);
+        setTimeout(function() {
+            waiting = false;
+            emitter.off('read_characteristic', cb);
+            reject({code:408, result:'read_characteristic Timeout'});
+        }, that.option.read_timeout);
 
         emitter.on('read_characteristic', cb);
         OneChat_readCharacteristic(charuuid);
